92def589266b6b3472549bfd6f727ea4c299e76a
Qualyyy <Jarne.QUALY@student.howest.be>
2025-08-08 16:45:07 +0200

    Add category filter and improve filter logic

--- docs/js/filter.js
+++ docs/js/filter.js
@@ -1,4 +1,4 @@
-document.getElementById('filter-list-owners').addEventListener('change', (event) => {
+document.getElementById('filter-list-owner').addEventListener('change', (event) => {
     if (event.target.name === 'owner') {
         updateVisibleRepos();
     }
@@ -12,3 +12,76 @@ function updateVisibleRepos() {
     });
 }
 
+document.getElementById('filter-list-category').addEventListener('change', (event) => {
+    if (event.target.name === 'category') {
+        updateVisibleCategories();
+    }
+});
+
+function updateVisibleCategories() {
+    const selectedCategories = Array.from(document.querySelectorAll('input[name="category"]:checked')).map(input => input.value);
+    document.querySelectorAll('.folder-container').forEach(categoryDiv => {
+        const category = categoryDiv.getAttribute('data-category');
+        categoryDiv.style.display = selectedCategories.length === 0 || selectedCategories.includes(category) ? '' : 'none';
+    });
+}
+
+function getOwners(data) {
+    const ownersSet = new Set();
+
+    for (const repoKey in data) {
+        const owner = repoKey.split('/')[0];
+        ownersSet.add(owner);
+    }
+
+    return Array.from(ownersSet).sort();
+}
+
+function getCategories(data) {
+    const categoriesSet = new Set();
+
+    for (const repoKey in data) {
+        const repo = data[repoKey];
+        for (const category in repo) {
+            categoriesSet.add(category);
+        }
+    }
+
+    return Array.from(categoriesSet);
+}
+
+
+function addCheckboxes(key, list) {
+    const filterList = document.getElementById(`filter-list-${key}`);
+    filterList.innerHTML = '';
+
+    for (const item of list) {
+        filterList.insertAdjacentHTML('beforeend', `<li><label><input type="checkbox" name="${key}" value="${item}">${item}</label></li>`)
+    }
+}
+
+async function addAllFilters() {
+    try {
+        const response = await fetch(`./components.json?ts=${Date.now()}`);
+        if (!response.ok) throw new Error('Failed to load components.json');
+        const components = await response.json();
+
+        const repoOwners = getOwners(components);
+        const categories = getCategories(components);
+
+        const filters = {
+            owner: repoOwners,
+            category: categories
+        };
+
+        for (const key in filters) {
+            console.log(key, filters[key]);
+            addCheckboxes(key, filters[key]);
+        }
+    }
+    catch (error) {
+        console.error('Error loading components:', error);
+    }
+}
+
+addAllFilters();
